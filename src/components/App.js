2c3a592feebd7d9d2ce1e1a59b324c6f599c8617
sepuckett86 <sepuckett86@gmail.com>
2019-08-13 10:32:19 -0700

    make components for dog

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Dog from './Dog';
+import Header from './Header';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+      <Dog />
+    </>
+  );
 }
