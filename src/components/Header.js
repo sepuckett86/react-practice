9fca222af29f9f5debe934fb1b9d7474213b8e19
sepuckett86 <sepuckett86@gmail.com>
2019-08-21 10:40:35 -0700

    more

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>React Router Practice</header>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" style={{ textDecoration: 'none' }}>Home Page</Link>
+          </li>
+          <li>
+            <Link to="/about" style={{ textDecoration: 'none' }}>About Page</Link>
+          </li>
+          <li>
+            <Link to="/contact" style={{ textDecoration: 'none' }}>Contact Page</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
