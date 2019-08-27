2c4f855d857d3799331a859b00b358ff4fbf64de
sepuckett86 <sepuckett86@gmail.com>
2019-08-27 09:44:17 -0700

    setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,5 @@
 import { combineReducers } from 'redux';
-import lunch from './lunchReducer';
-import doReducer from './doReducer';
 
 export default combineReducers({
-  lunch,
-  do: doReducer
+  reducer: 'me'
 });
--- src/selectors/lunchSelectors.js
+++
@@ -1,3 +0,0 @@
-export const getChips = state => state.lunch.chips;
-
-export const getTotalChips = state => getChips(state).length;
